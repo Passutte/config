4ef495d46f4eca8475e7d4f213007b08f6b59de8
Pascal Sutter <pascal@familysutter.ch>
2024-03-26 23:54:44 +0100

    [tmux] fish & tmux updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,12 +7,8 @@ set -gx EDITOR vim
 # bind Ctrl+h to accept the default suggestion
 bind \ch forward-char
 
-# make fzf search hidden files too
-set -U fzf_fd_opts --hidden --no-ignore --exclude .git
-fzf_configure_bindings --git_status=\cs --git_log=\cl --directory=\cf --processes=\cp
-
 # set tide prompts
-set --universal tide_left_prompt_items status context pwd git virtual_env
+set --universal tide_left_prompt_items status context pwd git
 set -U tide_right_prompt_items
 set -U tide_pwd_truncate_margin 1000000000
 
@@ -22,61 +18,3 @@ set PATH ~/.local/bin $PATH
 # # aliases
 alias sp="source ~/.config/fish/config.fish"
 source ~/.bash_aliases
-
-# code search utils
-function cgrep -d "search text in folder"
-  set -l options 'a/all'  # allow non git directories to be searched
-  argparse $options -- $argv
-
-  if not test $_flag_a  # then check if current dir is a git repo
-    if git rev-parse --show-toplevel > /dev/null 2>&1
-    else
-      echo "cgrep: [Warn] Not in a git repository, search not initiated." >&2
-      return
-    end
-  else
-    echo "cgrep: File - Line - Match (if any)" >&2
-    set _option_allow_non_git "--no-index"
-  end
-
-  set _cgrep_res $( \
-    git grep --line-number -I $_option_allow_non_git '' | fzf \
-    --delimiter ':' \
-    --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
-    --preview-window down,70%,~3,+{2}+3-/2 \
-    --border=sharp --layout=reverse)
-
-  set _cgrep_f $(echo $_cgrep_res | cut -d':' -f1)
-  set _cgrep_lineno $(echo $_cgrep_res | cut -d':' -f2)
-  set _cgrep_code $(echo $_cgrep_res | cut -d':' -f3-)
-
-  echo $_cgrep_f
-  echo $_cgrep_lineno
-  echo $_cgrep_code
-end
-
-function vgrep -d "search text in folder and open with vim"
-  set -l options 'a/all'  # allow non git directories to be searched
-  argparse $options -- $argv
-
-  if test $_flag_a
-    set _option_allow_non_git "-a"
-  end
-  set cgrep_res $(cgrep $_option_allow_non_git)
-  # the `cgrep` stdout result is an array[3]
-  # [1]: file name
-  # [2]: line number
-  # [3]: (might be blank) matched file content
-
-  if not test -f "$cgrep_res[1]"
-    echo "vgrep: [Warn] No valid file provided. No action performed."
-    return
-  end
-
-  # open file, navigate to line, highlight line
-  command vim +"$cgrep_res[2]" +'set cursorline' "$cgrep_res[1]"
-
-  for i in (seq 1 3)
-    echo "$cgrep_res[$i]"
-  end
-end
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ rm master.zip && \
 cd config-main
 ```
 
-* MacOS ($NAME should be the name after /Users/... and $USER is `echo $USER`):
+* MacOS (NAME should be the name after /Users/... and USER is `echo $USER`):
   ```
   sudo bash install.sh $NAME macOS $USER 2>&1 | tee /tmp/initial_setup.log
   ```
@@ -36,6 +36,33 @@ For Linux, key mappings config found in `ubuntu`
 guake --restore-preferences=guake.cfga
 guake --save-preferences=guake.cfg
 ```
+
+### Fish
+1. Enter fish shell
+    ```
+    fish
+    ```
+2. install fisher plugin manager ([ref](https://github.com/jorgebucaran/fisher))
+    ```
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+    ```
+3. install packages required by some plugins
+    * fonts required for `tide` ([ref](https://github.com/IlanCosman/tide#fonts))
+        * [MesloLGS NF Regular.ttf](https://github.com/IlanCosman/tide/blob/assets/fonts/mesloLGS_NF_regular.ttf?raw=true)
+        * [MesloLGS NF Bold.ttf](https://github.com/IlanCosman/tide/blob/assets/fonts/mesloLGS_NF_bold.ttf?raw=true)
+        * [MesloLGS NF Italic.ttf](https://github.com/IlanCosman/tide/blob/assets/fonts/mesloLGS_NF_italic.ttf?raw=true)
+        * [MesloLGS NF Bold Italic.ttf](https://github.com/IlanCosman/tide/blob/assets/fonts/mesloLGS_NF_bold_italic.ttf?raw=true)
+        * Also, set the terminal font to one of these
+
+4. install plugins with `fisher`
+    ```
+    fisher install IlanCosman/tide@v6
+    ```
+
+5. Set configuration
+    ```
+    tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time='24-hour format' --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='One line' --prompt_spacing=Compact --icons='Few icons' --transient=No
+    ```
 
 ### Additional Plugins
 * https://github.com/ohmyzsh/ohmyzsh/tree/master
